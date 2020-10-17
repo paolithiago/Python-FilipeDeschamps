b9e7acef3088021dc5b946303396734b82bdeb77
paolithiago <54008103+paolithiago@users.noreply.github.com>
2020-10-17 09:51:04 -0300

    Create lambda_alterar_linhas_Valores.py

---
+++ lambda_alterar_linhas_Valores.py
@@ -0,0 +1,4 @@
+df['Tipo de Alertas e Restrições'] = df['Tipo de Alertas e Restrições'].apply(lambda x:x.capitalize())
+df.head()
+
+https://colab.research.google.com/drive/17HaeRH-ZtgUgxGdDH37C-tSpnFBFrTql#scrollTo=0Ls0UVzlmeYd
