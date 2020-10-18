6f2572be06d1ee4704826525a025bf82e791851c
paolithiago <54008103+paolithiago@users.noreply.github.com>
2020-10-18 08:36:42 -0300

    Update AnaliseBasicaDataSetMod4PythonSigmoidal.py

--- AnaliseBasicaDataSetMod4PythonSigmoidal.py
+++ AnaliseBasicaDataSetMod4PythonSigmoidal.py
@@ -1,6 +1,9 @@
+#https://colab.research.google.com/drive/17HaeRH-ZtgUgxGdDH37C-tSpnFBFrTql#scrollTo=prR1dE_CXlDl
 # importando o pandas
 import pandas as pd
 
+  
+#________________________________ BLOCO TREINAMETO ______________________________________________________________
 df = pd.read_excel('https://www.dropbox.com/s/v0x8mbaygdqubli/contas_a_pagar.xlsx?dl=1')
 df.head()                       # EXTRAI LINHAS DO DATASET
 df                              # EXTRAI OS VALORES DO DATASET
@@ -19,3 +22,16 @@ print(df.isnull().sum())                                                        
 print(df.isnull().sum().sort_values(ascending = False)/df.shape[0]*100)                # CASOVALORES NULOS, SOMA PARA VER QUANTIDADE POR COLUNA, ORDENA E DIVIDE PELA QTDE DE LINHAS COM MULTP POR 100 PARA TER %
 df.groupby('pagamento')['valor'].sum().sort_values(ascending = False).plot.bar()      # IMPRIME UM GRAFICO BASICO COM SOMA DE PAGAMENTO POR TIPO DE VALOR  
 df['codigo_pagamento'].value_counts().plot.bar()                                      # IMPRIME UM GRAFICO DE BARRAS COM CONTAGEM DE VALORES POR CODIGO PAGAMENTO MOSTRANDO DUPLICIDADE  
+
+#_________________ ANALISE VALORES DUPLICADOS __________________________________________________________________________
+df.duplicated() # NESTE CASO GEREI O DUPLICATED PARA TODAS AS LINHAS 100% ENTAO TEMOS A LINHA 7 DUPLICADA.  ANALISANDO O BASE, A LINHA 7 E EXATAMENTE IGUAL A LINHA 6
+df.duplicated(subset = 'codigo_pagamento') # AGORA A ANALISE E SOBRE A COLUNA CODIGO PAGAMENTO, SOMENTE A UNICA COLUNA, O QUE ENTENDE E QUE TEMOS MAIS DE UM PAGAMENTO COM O MESMO CODIGO DE BARRAS
+
+# NESTE CASO REMOVEMOS A LINHA QUE ESTAVA 100% DUPLICADA
+
+df.drop_duplicates(inplace=True)
+df.duplicated()
+
+# NESTE CASO USANDO O DROP DUPLICATES COM SUBSET E INPLACE REMOVEMOS AS LINHAS QUE TINHAM O CODIGO DE BARRAR DUPLICADOS, E NAO 100% D LINHA
+df.drop_duplicates(subset='codigo_pagamento', inplace=True)
+df.drop_duplicates
