b7e0cf453525ca0f9377425f4e46abc5de55e4a0
paolithiago <54008103+paolithiago@users.noreply.github.com>
2020-10-20 14:37:10 -0300

    Update SomaValoresPorColuna(Grouby_comOrdenacao).py

--- SomaValoresPorColuna(Grouby_comOrdenacao).py
+++ SomaValoresPorColuna(Grouby_comOrdenacao).py
@@ -1,1 +1,2 @@
 df.groupby('Estação')['Entradas'].sum().sort_values(ascending = False)            # ORDENA VALORES SOMANDO DO MAIOR P/MENOR
+df.groupby('Estação')['Entradas'].sum().sort_values(ascending = False).plot.bar() # COM GRAFICO
